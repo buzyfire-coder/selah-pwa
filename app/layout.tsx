857af41b0748085a5b76f16b5d7ba3770fac0d1d
buzyfire-coder <buzyfire@gmail.com>
2026-01-14 00:26:20 +0000

    layout

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,16 +1,7 @@
 "use client";
 
 import "./globals.css";
-import type { Metadata } from "next";
-import { Inter } from "next/font/google";
 import { useEffect } from "react";
-
-const inter = Inter({ subsets: ["latin"] });
-
-export const metadata: Metadata = {
-  title: "Selah",
-  description: "一個幫助人親近神的屬靈空間",
-};
 
 export default function RootLayout({
   children,
